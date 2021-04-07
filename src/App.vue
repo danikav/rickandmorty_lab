ade91e81c12121b3a8a1fc64cbacbd802704a34a
Dani <danikav1989@gmail.com>
2021-04-07 13:16:17 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Characters</h1>
+    <section>
+      <characters-list :characters='characters'></characters-list>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import CharactersList from './components/CharactersList.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      characters: [],
+      selectedCharacter: null,
+      new_characters: []
+      // url = 'https://rickandmortyapi.com/api/character/'
+    }
+  },
+
+  mounted() {
+    // fetch('https://rickandmortyapi.com/api/character/')
+    // .then(res => res.json())
+    // .then(characters => this.characters = characters)
+    // fetch(this.characters.info.next)
+    // .then(res => res.json())
+    // .then(characters => this.new_characters = characters)
+  let url = 'https://rickandmortyapi.com/api/character/'
+    while (url) {
+      fetch(url)
+      .then(res => res.json())
+      .then(responseData => {
+        this.characters.push(...responseData.results)
+        url = responseData.info.next
+      })
+      
+  // this.fetchCharacters()
+  }
+  },
+
+    // fetchCharacters: function() {
+    
+    //  let url = 'https://rickandmortyapi.com/api/character/';
+
+    //  do {
+    //    const res = await fetch(url);
+    //    const data = await res.json();
+    //    url = data.next;
+    //    characters.push(...data.characters);
+    //  } while(url)
+    // let url = 'https://rickandmortyapi.com/api/character/'
+    // while (url.info.next !== null) {
+    //   fetch(url)
+    //   .then(res => res.json)
+    //   .then(characters => this.characters.push(characters))
+    //   url = url.info.next
+    // }
+    
   components: {
-    HelloWorld
+  "characters-list": CharactersList
   }
 }
 </script>
---
+++ src/components/CharactersList.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+      <ul>
+          <list-item v-for="(character, index) in characters.results" :character="character" :key="index"> </list-item>
+      </ul>
+  </section>
+</template>
+
+<script>
+
+import ListItem from './ListItem.vue';
+
+export default {
+    name: 'characters-list',
+    props: ['characters'],
+    components: {
+        "list-item" : ListItem
+    }
+}
+</script>
+
+<style>
+
+</style>
